d877077eeffe18023d493135495f490840c48575
Gyanvi <gyanvi0109@gmail.com>
2024-01-30 09:28:39 +0530

    first commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using ecom.Data;
+using ecom.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecom.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+                _context = context; 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await _context.Products.ToListAsync());
+
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+
+        public async Task<IActionResult> GetProduct([FromRoute] int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using ecom.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecom.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
